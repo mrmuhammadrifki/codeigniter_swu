2d1182347671257b32e755b19fa5d5f687c40935
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-04-06 15:45:25 +0700

    first commit

--- latihan_templating_ci3/application/config/config.php
+++ latihan_templating_ci3/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/latihan_templating_ci3/';
+$config['base_url'] = 'http://localhost/wpl_codeigniter/latihan_templating_ci3/';
 
 /*
 |--------------------------------------------------------------------------
